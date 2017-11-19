f44239259e56d3685560348fc33972bdab036e19
oktemer <oktemerbay@gmail.com>
2017-11-19 14:22:06 +0300

    Grid control not finished yet

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
 
+    public static float gridWidth = 10;
+    public static float gridHeight = 20;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +16,21 @@ public class Game : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private static bool checkForXAxis(Vector3 position)
+    {
+        float position_x = position.x;
+        return false;
+    }
+
+    private static bool checkForYAxis(Vector3 position)
+    {
+        return false;
+    }
+
+    public static bool IsInGrid(Vector3 position)
+    {
+        return false;
+    }
+
 }

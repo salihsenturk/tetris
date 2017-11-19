c715338b19ebcf2bf4765ef58e8acee6f26cab07
Salih Şentürk <sal.senturk@gmail.com>
2017-11-20 00:06:53 +0200

    added box collider to grid and some prefabs

--- Assets/Scripts/Tetrisform.cs
+++ Assets/Scripts/Tetrisform.cs
@@ -6,6 +6,7 @@ public class Tetrisform : MonoBehaviour {
 
     float fall = 0;
     float fallSpeed = 1;
+    bool stopGoingDown = false;
 
      // Use this for initialization
     void Start () {
@@ -22,22 +23,23 @@ public class Tetrisform : MonoBehaviour {
         
         if (Input.inputString != null)
         {
-            Debug.Log("The key entered is " + Input.inputString.ToString());
+            //Debug.Log("The key entered is " + Input.inputString.ToString());
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1,0,0);
-        }else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1,0,0);
-        }else if (Input.GetKeyDown(KeyCode.DownArrow))
+        }else if (!stopGoingDown && Input.GetKeyDown(KeyCode.DownArrow))
         {
             transform.position += new Vector3(0,-1,0);
         }else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             transform.Rotate(new Vector3(0, 0, 90));
-        }else if (GoDown())
+        }else if (!stopGoingDown && GoDown())
         {
             transform.position += new Vector3(0, -1,0);
         }
@@ -50,5 +52,11 @@ public class Tetrisform : MonoBehaviour {
         bool isGoDown = myTime - fall > fallSpeed;
         fall = isGoDown ? myTime:fall;
         return isGoDown;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(!collision.gameObject.name.Equals("Left Wall") && !collision.gameObject.name.Equals("Right Wall") && collision.gameObject.transform.position.y < gameObject.transform.position.y) {
+            stopGoingDown = true;
+        }
     }
 }

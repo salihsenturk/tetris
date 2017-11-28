1abaff9463f230e5da8a802f4dd26785fc6d1fbd
Salih Şentürk <sal.senturk@gmail.com>
2017-11-28 18:49:19 +0200

    physics & collision

--- Assets/Scripts/Tetrisform.cs
+++ Assets/Scripts/Tetrisform.cs
@@ -7,8 +7,9 @@ public class Tetrisform : MonoBehaviour {
     float fall = 0;
     float fallSpeed = 1;
     bool stopMoving = false;
+    bool leftOff = false, rightOff = false;
 
-     // Use this for initialization
+    // Use this for initialization
     void Start () {
 		
 	}
@@ -26,11 +27,12 @@ public class Tetrisform : MonoBehaviour {
                 //Debug.Log("The key entered is " + Input.inputString.ToString());
             }
 
-            if (Input.GetKeyDown(KeyCode.RightArrow)) {
-                transform.position += new Vector3(1, 0, 0);
-
-            } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-                transform.position += new Vector3(-1, 0, 0);
+            if (!rightOff && Input.GetKeyDown(KeyCode.RightArrow)) {
+                transform.position += new Vector3(.5f, 0, 0);
+                leftOff = false;
+            } else if (!leftOff && Input.GetKeyDown(KeyCode.LeftArrow)) {
+                transform.position += new Vector3(-.5f, 0, 0);
+                rightOff = false;
             } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
                 transform.position += new Vector3(0, -1, 0);
             } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
@@ -50,10 +52,37 @@ public class Tetrisform : MonoBehaviour {
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
-        Debug.Log(collision.gameObject.name);
-        if(!collision.gameObject.name.Equals("Left Wall") && !collision.gameObject.name.Equals("Right Wall") && collision.gameObject.transform.position.y < gameObject.transform.position.y) {
+        Debug.Log(collision.gameObject.name + " collision");
+        Debug.Log(collision.contacts.Length);
+
+        if (collision.gameObject.name.Equals("Left Wall")) {
+            leftOff = true;
+        } else if (collision.gameObject.name.Equals("Right Wall")) {
+            rightOff = true;
+        } else if (collision.gameObject.name.Equals("Floor") || collision.gameObject.transform.position.y < gameObject.transform.position.y) {
             stopMoving = true;
-            enabled = false;  
+            this.enabled = false;
+        }
+
+        for (int i = 0; i < collision.contacts.Length; i++) {
+            Debug.Log(collision.contacts[0].normal);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger) {
+        Debug.Log(trigger.gameObject.name + " trigger");
+        if (trigger.gameObject.name == "Floor") {
+            stopMoving = true;
+        }
+
+        if (trigger.gameObject.name.Equals("Left Wall")) {
+            leftOff = true;
+        }
+        if (trigger.gameObject.name.Equals("Right Wall")) {
+            rightOff = true;
+        }
+        if (!trigger.gameObject.name.Equals("Left Wall") && !trigger.gameObject.name.Equals("Right Wall") && trigger.gameObject.transform.position.y < gameObject.transform.position.y) {
+            stopMoving = true;
         }
     }
 
